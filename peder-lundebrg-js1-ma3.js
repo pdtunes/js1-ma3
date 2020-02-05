2592da14cda06cf10d993840689d2f4d77a747ba
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-02-05 13:05:39 +0100

    Create peder-lundebrg-js1-ma3.js

---
+++ peder-lundebrg-js1-ma3.js
@@ -0,0 +1,84 @@
+/* Level 1
+Question 1
+Refer: lesson 1
+Convert the function below to an arrow function:
+
+function(a, b) {
+    return a - b;
+}
+ */
+
+
+(a, b) => {
+    return a - b
+}
+
+/* Question 2
+Refer: lesson 2 / 4
+Make a call to the URL below, pass the JSON it returns to a function and inside that function loop through the results and log each game's name.
+
+In the catch method of your code, redirect to error.html if there is an error.
+
+https://api.rawg.io/api/games?genres=sports
+ */
+
+
+
+/* Question 3
+Refer: lesson 3
+Replace the word cats with the word giraffes in the following sentence:
+
+These cats are outrageous. */
+
+
+
+/* Question 4
+Refer: lesson 4
+Given the URL below, write code that checks if there is a userId parameter in the query string.
+
+If there is no userID parameter, redirect the browser to third.html.
+
+If there is a userID parameter and its value is less than 10, redirect to first.html.
+
+If there is a userID parameter and its value is 10 or greater, redirect to second.html.
+
+https://my.site.com?userId=7 */
+
+
+
+/* Question 5
+Refer: lesson 4
+Write code that removes the button, and only the button, from its parent element in the HTML below:
+
+<div class="container">
+    <p>Lorem ipsum dolor</p>
+    <button class="btn">Click me</button>
+</div> */
+
+
+
+
+
+/* Question 6
+Refer: lesson 4
+Create an li element with a text value of Parrots and a class of parrots.
+
+Add the new item as the second item in the ul below (add it after Cows).
+
+<ul class="animals">
+    <li class="cows">Cows</li>
+    <li class="elephants">Elephants</li>
+</ul> */
+
+
+
+/* Question 7
+Refer: lesson 4
+Make a call to the URL below and pass the JSON it returns to a function.
+
+Inside that function select the div from the HTML below and assign the rating property from the JSON object as it's text value.
+
+In the catch method, log the error that may be returned.
+
+https://api.rawg.io/api/games/3801
+<div class="rating"></div> */
